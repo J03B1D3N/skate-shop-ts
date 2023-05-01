94b90e3420e3a1e78539c028b27ed788f88fc543
Justas Lapiene <justas63@gmail.com>
2023-05-01 18:37:02 +0200

    setting up gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='/skate-shop-ts'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
