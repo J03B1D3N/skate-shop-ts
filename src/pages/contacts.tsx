2ec9a0bdc9c1512bc3a2d1b2745a01cc157714e9
Justas Lapiene <justas63@gmail.com>
2023-05-01 16:38:53 +0200

    finished styling the pages

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -5,9 +5,11 @@ export default function Contacts() {
             <p>FAX: 202-456-2461</p>
             <p>SwitchBoard: 202-456-1414</p>
         </div>
-        <div className="adress"><p>The White House</p>
-                                <p>1600 Pennsylvania Avenue NW</p>
-                                <p>Washington, DC 20500</p>
-                                </div>
+        <div className="adress">
+            <p>The White House</p>
+            <p>1600 Pennsylvania Avenue NW</p>
+            <p>Washington, DC 20500</p>
+        </div>
+        <img className="map" src="/images/maps.webp" alt="map" />
     </div>
 }
