8862b8ebc3d3b8efbaf5c27ab667be71ca33b61b
Justas Lapiene <justas63@gmail.com>
2023-05-05 19:35:36 +0200

    added a send us a letter section in the contacts page

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,17 +1,57 @@
+import { useState } from "react"
+
 export default function Contacts() {
+    
+    const [sent, setSent] = useState(false)
+
+
+
+
+    function handleSubmit(e:any) {
+        e.preventDefault()
+        setSent(true)
+    }
+
     return <div className="contacts">
-        <img className="map" src="./images/maps.webp" alt="map" />
-        <div className="wrapper">
-            <div className="phone">
-                <p>Phone: 202-456-1111</p>
-                <p>FAX: 202-456-2461</p>
-                <p>SwitchBoard: 202-456-1414</p>
+        <div className="details">
+            <img className="map" src="./images/maps.webp" alt="map" />
+            <div className="wrapper">
+                <div className="phone">
+                    <p>Phone: 202-456-1111</p>
+                    <p>FAX: 202-456-2461</p>
+                    <p>SwitchBoard: 202-456-1414</p>
+                </div>
+                <div className="adress">
+                    <p>The White House</p>
+                    <p>1600 Pennsylvania Avenue NW</p>
+                    <p>Washington, DC 20500</p>
+                </div>
             </div>
-            <div className="adress">
-                <p>The White House</p>
-                <p>1600 Pennsylvania Avenue NW</p>
-                <p>Washington, DC 20500</p>
-            </div>
+        </div>
+        <div className="contactUs">
+            <div className="title">SEND US A LETTER</div>
+            
+            <form action="POST" onSubmit={(e) => {handleSubmit(e)}}>
+                {!sent? <>
+                <label htmlFor="firstName">What is your name?</label>
+                <input type="text" id="firstName" required/>
+
+                <label htmlFor="lastName">What is your last name?</label>
+                <input type="text" id="lastName" required/>
+
+                <label htmlFor="email">What is your email?</label>
+                <input type="text" id="email" required/>
+
+                <label htmlFor="qq">What is your question or complaint?</label>
+                <textarea id="qq" required/>
+                <button>SUBMIT</button>
+                </>
+                :<div className="letterSent">
+                    <p>Thank you for your letter!</p>
+                    <p>An email confirmation should be sent to your inbox shortly</p>
+                </div>}
+                
+            </form>
         </div>
     </div>
 }
